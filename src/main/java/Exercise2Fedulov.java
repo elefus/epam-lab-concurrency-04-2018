1755b8217a7a5cf60225896a0c54f30acd864e52
mercenery@inbox.ru <mercenery@inbox.ru>
2018-04-06 15:31:51 +0300

    Concurrency.Exercices.ecercise2.reader-writer-storageDone(Fedulov)

--- src/main/java/Exercise2Fedulov.java
+++ src/main/java/Exercise2Fedulov.java
@@ -1,8 +1,9 @@
 import lombok.SneakyThrows;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Exercise2Fedulov {
     static class Storage {
@@ -44,31 +45,32 @@ public class Exercise2Fedulov {
         Storage storage = new Storage("\n------------------initial string-----------------\n");
         System.out.println(storage.getSharedString());
 
-        Thread[] threads = new Thread[4];
+        Writer       writer  = new Writer(storage);
+        List<Reader> readers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            readers.add(new Reader("reader # " + i, storage));
+        }
 
-        Writer writer      = new Writer(storage);
-        Reader readerOne   = new Reader(storage);
-        Reader readerTwo   = new Reader(storage);
-        Reader readerThree = new Reader(storage);
+        List<Thread> threads = new ArrayList<>(readers.size() + 1);
 
-        threads[0] = new Thread(writer);
-        threads[1] = new Thread(readerOne);
-        threads[2] = new Thread(readerTwo);
-        threads[3] = new Thread(readerThree);
+        for (int i = 0; i < threads.size(); i++) {
+            threads.add(new Thread(readers.get(i)));
+            threads.get(i).setPriority(Thread.MIN_PRIORITY);
+        }
+        Thread writerThread = new Thread(writer);
+        writerThread.setPriority(Thread.MAX_PRIORITY);
+        threads.add(writerThread);
 
-        threads[0].setPriority(Thread.MAX_PRIORITY);
-        threads[1].setPriority(Thread.MIN_PRIORITY);
-        threads[2].setPriority(Thread.MIN_PRIORITY);
-        threads[3].setPriority(Thread.MIN_PRIORITY);
 
-        for (int i = 0; i < threads.length; i++) {
-            threads[i].start();
+        for (Thread t :
+                threads) {
+            t.start();
         }
     }
 }
 
 class Writer implements Runnable {
-    ReentrantLock            lock = new ReentrantLock();
+    //    ReentrantLock            lock = new ReentrantLock();
     Exercise2Fedulov.Storage storageWriters;
 
     Writer(Exercise2Fedulov.Storage storageWriters) {
@@ -83,7 +85,7 @@ class Writer implements Runnable {
                 System.out.println("\n...writer wait for writing\n");
                 TimeUnit.MILLISECONDS.sleep(3000);
             }
-            lock.lock();
+//            lock.lock();
             {
                 storageWriters.setWriting(true);
                 storageWriters.setSharedString(String.valueOf(new Date()));
@@ -91,16 +93,31 @@ class Writer implements Runnable {
                 System.out.println("\nwriter wrote string = " + storageWriters.getSharedString() + "\n");
             }
             storageWriters.setWriting(false);
-            lock.unlock();
+//            lock.unlock();
         }
     }
 }
 
 
 class Reader implements Runnable {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Reader(String name, Exercise2Fedulov.Storage storage) {
+        this.name = name;
+        this.storage = storage;
+    }
+
     Exercise2Fedulov.Storage storage;
     String                   readstring    = null;
-    ReentrantLock            reentrantLock = new ReentrantLock();
+//    ReentrantLock            reentrantLock = new ReentrantLock();
 
     public Reader(Exercise2Fedulov.Storage storage) {
         this.storage = storage;
@@ -114,15 +131,15 @@ class Reader implements Runnable {
                 System.out.println("\nreader wait for read... \n");
                 TimeUnit.MILLISECONDS.sleep(500);
             }
-            reentrantLock.lock();
+//            reentrantLock.lock();
             {
                 storage.setReading(true);
                 readstring = storage.getSharedString();
                 TimeUnit.MILLISECONDS.sleep(1000);
-                System.out.println("\nreader read th string = " + readstring + "\n");
+                System.out.println("\nreader " + getName() + "read th string = " + readstring + "\n");
             }
             storage.setReading(false);
-            reentrantLock.unlock();
+//            reentrantLock.unlock();
         }
     }
 }
